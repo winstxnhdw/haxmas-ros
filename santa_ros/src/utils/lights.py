2ee30c3d5bf6a480934056fd7af36d85f1fb0de5
winstnxhdw <winstxnhdw@gmail.com>
2020-12-23 17:26:38 +0800

    actually works now

--- santa_ros/src/utils/lights.py
+++ santa_ros/src/utils/lights.py
@@ -3,49 +3,50 @@ from time import *
 
 red = LED(22) 
 yellow = LED(27) 
-green = LED(17) 
+green = LED(17)
+
 duration = 0 
 delay = 0.1 
 
-def Red(duration): 
+def flash_red(duration): 
 
     start = time() 
     stop = time() + duration 
 
-    while (time()<stop): 
+    while (time() < stop): 
         red.on() 
         sleep(delay) 
         red.off() 
         sleep(delay) 
         
-def Yellow(duration): 
+def flash_orange(duration): 
 
     start = time() 
     stop = time() + duration 
     
-    while (time()<stop): 
+    while (time() < stop): 
         yellow.on() 
         sleep(delay) 
         yellow.off() 
         sleep(delay) 
         
-def Green(duration): 
+def flash_green(duration): 
 
     start = time() 
     stop = time() + duration 
     
-    while (time()<stop): 
+    while (time() < stop): 
         green.on() 
         sleep(delay) 
         green.off() 
         sleep(delay) 
         
-def Mix(duration): 
+def flash_all(duration): 
     
     start = time() 
     stop = time() + duration 
     
-    while (time()<stop): 
+    while (time() < stop): 
         green.on() 
         red.on() 
         sleep(delay) 
@@ -59,29 +60,4 @@ def Mix(duration):
         green.off() 
         red.on() 
         yellow.off() 
-        sleep(delay) 
-        
-def Clear(): 
-    
-    red.off() 
-    yellow.off() 
-    green.off() 
-    
-    while 1: 
-        Yellow(1) 
-        Green(1) 
-        Red(1) 
-        Mix(5) 
-        Clear()
-
-def flash_red():
-    pass
-
-def flash_orange():
-    pass
-
-def flash_green():
-    pass
-
-def flash_all():
-    pass
+        sleep(delay)
